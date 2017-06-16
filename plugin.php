05d702f9fdaef6033e8f36a184ff2ebea7ae545e
ksherlock <ksherlock@gmail.com>
2017-06-16 13:41:59 -0400

    WIP - update domain checking to allow subdomains. (#1)
    * Update plugin.php
    
    * store domain list as an array.

--- plugin.php
+++ plugin.php
@@ -18,24 +18,55 @@ if (!yourls_is_admin())
 // Hook the admin page into the 'plugins_loaded' event
 yourls_add_action( 'plugins_loaded', 'panthro_whitelist_domain_add_page' );
 
+
+function panthro_string_to_list($string) {
+
+	$list = preg_split('/\r?\n/', $string);
+	$rv = array();
+	foreach ($list as $x) {
+		$x = trim($x);
+		if ($x === "") continue;
+		$x = strtolower($x);
+		array_push($rv, $x);
+	}
+	return $rv;
+}
+
+function panthro_domain_list() {
+	// return domain list as an array.
+	$list = yourls_get_option ('panthro_whitelist_domain_list', '');
+	if ($list === "") return array();
+	$list = unserialize($list);
+	if (is_array($list)) return $list;
+	
+	return $panthrow_string_to_list($list);
+}
+
+
 // Get whitelisted domains from YOURLS options feature and compare with current domain address
 function panthro_whitelist_domain_root ( $bol, $url ) {
-	$return = false;
 
 	//if (yourls_is_admin()) return $return;
 
-	$domain = str_ireplace('www.', '', parse_url($url, PHP_URL_HOST));
-	$domain_list = yourls_get_option ('panthro_whitelist_domain_list');
+	$domain = strtolower(parse_url($url, PHP_URL_HOST));
+
+	$domain_list = panthro_domain_list();
 	if ( $domain_list ) {
-		$domain_list_display = unserialize ( $domain_list );
-		//if (!in_array($domain, $domain_list_display)) {
-		if (strpos($domain_list_display,$domain) === false) {
-			$return['status']    = 'fail';
-			$return['code']      = 'error:domain-not-allowed';
-			$return['message']   = 'This domain is not allowed';
-			$return['errorCode'] = '400';
+
+		foreach ($domain_list as $needle) {
+			if ($needle === "") continue;
+			// exact match
+			if ($domain === $needle) return false;
+			// domain = www.example.com, $needle == example.com
+			$needle = ".$needle"
+			if (substr($domain, -strlen($needle)) === $needle) return false;
 		}
 	}
+
+	$return['status']    = 'fail';
+	$return['code']      = 'error:domain-not-allowed';
+	$return['message']   = 'This domain is not allowed';
+	$return['errorCode'] = '400';
 	return $return;
 }
 
@@ -53,15 +84,13 @@ function panthro_whitelist_domain_do_page () {
 	}
 }
 
+
 // Display form to administrate whitelisted domains list
 function panthro_whitelist_domain_form () {
 	$nonce = yourls_create_nonce( 'whitelist_domain' ) ;
-	$domain_list = yourls_get_option ('panthro_whitelist_domain_list','Enter domain addresses here, one per line');
-	if ($domain_list != 'Enter domain addresses here, one per line' ){
-		$domain_list_display = unserialize ( $domain_list );
-	}else{
-		$domain_list_display = $domain_list;
-	}
+	$domain_list = panthro_domain_list();
+	$domain_list_display = implode("\r\n", $domain_list);
+
 	echo <<<HTML
 		<h2> WhiteList domains</h2>
 		<form method="post">
@@ -70,7 +99,9 @@ function panthro_whitelist_domain_form () {
 		<input type="hidden" name="nonce" value="$nonce" />
 		
 		<p>Whitelist following domains
-		<textarea cols="30" rows="5" name="whitelist_form">$domain_list_display</textarea>
+		<textarea cols="30" rows="5" name="whitelist_form"
+		placeholder="Enter domain addresses here, one per line"
+		>$domain_list_display</textarea>
 		</p>
 		
 		<p><input type="submit" value="Save" /></p>
@@ -83,8 +114,8 @@ function panthro_whitelist_domain_process () {
 	// Check nonce
 	yourls_verify_nonce( 'whitelist_domain' ) ;
 	// Update list
-	$sent_list = serialize($_POST['whitelist_form']);
-	yourls_update_option ( 'panthro_whitelist_domain_list',$sent_list );
+	$list = serialize(panthro_string_to_list($_POST['whitelist_form']));
+	yourls_update_option ( 'panthro_whitelist_domain_list',$list );
 	echo "White list updated" ;
 }
 ?>
