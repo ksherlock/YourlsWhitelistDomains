da27cd81ea6c12c19ca29f328c26470a34c86241
ksherlock <ksherlock@gmail.com>
2017-06-16 14:23:01 -0400

    remove $

--- plugin.php
+++ plugin.php
@@ -39,7 +39,7 @@ function panthro_domain_list() {
 	$list = unserialize($list);
 	if (is_array($list)) return $list;
 	
-	return $panthrow_string_to_list($list);
+	return panthrow_string_to_list($list);
 }
 
 
@@ -114,7 +114,8 @@ function panthro_whitelist_domain_process () {
 	// Check nonce
 	yourls_verify_nonce( 'whitelist_domain' ) ;
 	// Update list
-	$list = serialize(panthro_string_to_list($_POST['whitelist_form']));
+	$list = serialize(
+		($_POST['whitelist_form']));
 	yourls_update_option ( 'panthro_whitelist_domain_list',$list );
 	echo "White list updated" ;
 }
